1277ff44594eb8d05c991035e43e24259f7162db
Sachin <skad5455@gmail.com>
2021-01-29 14:06:15 +0530

    Added methods to compute stalemate, draw and filter moves by piece

--- common/helper_functions.py
+++ common/helper_functions.py
@@ -1,144 +1,168 @@
-import imp
-import json
 import copy
 
-rules = imp.load_source('chess_basic_rules','./common/rules.py')
-piece_value = json.load(open("./common/chess_piece_priority.json"))
-
-opposite = {  "white" :"black" , "black": "white" }
+from common.constants import PIECE_VALUE, OPPOSITE
+from common import rules
 
 
-def game_over(board,color):
-  if in_checkmate(board,color) or in_checkmate(board,opposite[color]) : return True
+def game_over(board, color):
+  """stalemate and draw"""
+  if in_checkmate(board, color) or in_checkmate(board, OPPOSITE[color]):
+    return True
   return False
 
 
-def get_moves(board,color):
+def get_moves(board, color, filter_piece = None):
   moves_list  = []
   moves = []
 
-  for x in board[color].keys():
-    if   "king"   in x:
-      moves = rules.legal_king_moves(board,color,x)
-    elif "queen"  in x:
-      moves = rules.legal_queen_moves(board,color,x)
-    elif "bishop" in x:
-      moves = rules.legal_bishop_moves(board,color,x)
-    elif "knight" in x:
-      moves = rules.legal_knight_moves(board,color,x)
-    elif "rook"   in x:
-      moves = rules.legal_rook_moves(board,color,x)
-    elif "pawn"   in x:
-      moves = rules.legal_pawn_moves(board,color,x)
+  for piece in board[color].keys():
+    if "king" in piece:
+      moves = rules.legal_king_moves(board, color, piece)
+    elif "queen" in piece:
+      moves = rules.legal_queen_moves(board, color, piece)
+    elif "bishop" in piece:
+      moves = rules.legal_bishop_moves(board, color, piece)
+    elif "knight" in piece:
+      moves = rules.legal_knight_moves(board, color, piece)
+    elif "rook" in piece:
+      moves = rules.legal_rook_moves(board, color, piece)
+    elif "pawn" in piece:
+      moves = rules.legal_pawn_moves(board, color, piece)
     
     for move in moves:
-      moves_list = moves_list + [ {"color": color,"piece":x,"new_position":move}  ]
-  
-  return moves_list
+      moves_list = moves_list + [{"color": color, "piece": piece, "new_position": move}]
+
+  return [move for move in moves_list if not filter_piece or move["piece"] == filter_piece]
 
 
 def get_reverse_map(board):
-
   white = {}
   black = {}
 
   for k,v in board["white"].items():
-    white[(v[0],v[1])]= k
+    white[(v[0], v[1])]= k
 
   for k,v in board["black"].items():
-    black[(v[0],v[1])]= k
+    black[(v[0], v[1])]= k
 
-  return { "white":white , "black":black}
-
-
-
-def if_piece_under_attack(board, color,piece):
-        oc = opposite[color]
-        moves = []
-
-        for x in board[oc].keys():
-                if   "king"   in x:
-                        moves = moves +  rules.legal_king_moves(board,oc,x)
-                elif "queen"  in x:
-                        moves = moves +  rules.legal_queen_moves(board,oc,x)
-                elif "bishop" in x:
-                        moves = moves +  rules.legal_bishop_moves(board,oc,x)
-                elif "knight" in x:
-                        moves = moves +  rules.legal_knight_moves(board,oc,x)
-                elif "rook"   in x:
-                        moves = moves +  rules.legal_rook_moves(board,oc,x)
-                elif "pawn"   in x:
-                        moves = moves +  rules.legal_pawn_moves(board,oc,x)
-
-        if board[color][piece] in moves:
-                return True
-
-        return False
+  return { "white": white, "black": black }
 
 
-def generate_board(board,move):
+def if_piece_under_attack(board, color, piece):
+  oc = OPPOSITE[color]
+  moves = []
+
+  for x in board[oc].keys():
+    if "king" in x:
+      moves = moves + rules.legal_king_moves(board, oc, x)
+    elif "queen" in x:
+      moves = moves + rules.legal_queen_moves(board, oc, x)
+    elif "bishop" in x:
+      moves = moves + rules.legal_bishop_moves(board, oc, x)
+    elif "knight" in x:
+      moves = moves + rules.legal_knight_moves(board, oc, x)
+    elif "rook" in x:
+      moves = moves + rules.legal_rook_moves(board, oc, x)
+    elif "pawn" in x:
+      moves = moves + rules.legal_pawn_moves(board, oc, x)
+
+  if board[color][piece] in moves:
+    return True
+
+  return False
+
+
+def generate_board(board, move):
   new_board = copy.deepcopy(board)
+
   killed_piece = None
-  for k,v in new_board[opposite[move['color']]].items(): 
-    if move['new_position'] == v :
+  for k,v in new_board[OPPOSITE[move['color']]].items(): 
+    if move['new_position'] == v:
       killed_piece = k
 
-  if killed_piece and killed_piece in new_board[opposite[move['color']]].keys() : del new_board[opposite[move['color']]][killed_piece]
+  if killed_piece and killed_piece in new_board[OPPOSITE[move['color']]].keys():
+    del new_board[OPPOSITE[move['color']]][killed_piece]
 
   new_board[move['color']][move['piece']] = move['new_position']
   return new_board
 
 
-def in_check(board,color):
-  oc = opposite[color]
-  moves = []
-  for x in board[oc].keys():
-    if   "king"   in x:
-      moves = moves +  rules.legal_king_moves(board,oc,x)
-    elif "queen"  in x:
-      moves = moves +  rules.legal_queen_moves(board,oc,x)
-    elif "bishop" in x:
-      moves = moves +  rules.legal_bishop_moves(board,oc,x)
-    elif "knight" in x: 
-      moves = moves +  rules.legal_knight_moves(board,oc,x)
-    elif "rook"   in x:
-      moves = moves +  rules.legal_rook_moves(board,oc,x)
-    elif "pawn"   in x:
-      moves = moves +  rules.legal_pawn_moves(board,oc,x)
-  if 'king' in board[color].keys():
-    if board[color]['king'] in moves:
+def in_check(board, color):
+  moves = get_moves(board, OPPOSITE[color])
+
+  for move in moves:
+    if board[color]['king'] == move['new_position']:
       return True
 
   return False
 
 
+def checked_by(board, color):
+  moves = get_moves(board, color)
+  checking_pieces = []
 
-def in_checkmate(board,color):
-   #TODO Here I have only condered the moves of king, and checked if any his any move can help him or not,testing it is remaining
-    
-  #if moves of the king are null, then it is surrounded by its own pieces and is safe.
+  for move in moves:
+    if board[color]['king'] == move['new_position']:
+      checking_pieces.append(move["piece"])
 
-  if in_check(board,color) == False:
+  return checking_pieces
+
+
+def in_checkmate(board, color):
+  if in_check(board, color) is False:
     return False
 
-  for p in get_moves(board,color):
-    if in_check(generate_board(board,p),color) == False:
+  for move in get_moves(board, color):
+    if in_check(generate_board(board, move), color) is False:
       return False
 
   return True
 
 
-def shielding(board,color,piece):
-  ## Shielding is how many pieces it's protecting from getting attacked, 
-  ## one way to calculate it is if it's occupying a position which
+def in_stalemate(board, color_to_play):
+  """If there are no moves for any of the armies and there is no check, it's a stalemate! """
+  moves = get_moves(board, color_to_play)
+  return len(moves) == 0 and in_check(board, color_to_play) is False
+
+
+def is_draw(board):
+  num_pieces_1 = len(board[color].keys())
+  num_pieces_2 = len(board[OPPOSITE[color]].keys())
+
+  if num_pieces_1 > 2 or num_pieces_2 > 2:
+    return False
+  elif (
+    num_pieces_1 <= 2 and (
+      board[color].get("rook_1") or
+      board[color].get("rook_2") or
+      board[color].get("queen") or
+      len([piece for piece in board[color].keys() if "pawn" in piece])
+    )
+  ):
+      return False
+  elif (
+    board[OPPOSITE[color]].get("rook_1") or
+    board[OPPOSITE[color]].get("rook_2") or
+    board[OPPOSITE[color]].get("queen") or
+    len([piece for piece in board[OPPOSITE[color]].keys() if "pawn" in piece])
+  ):
+    return False
+
+  return True
+
+
+def shielding(board, color, piece):
+  """Shielding is how many pieces it's protecting from getting attacked, 
+  one way to calculate it is if it's occupying a position which"""
     
-  if piece not in board[color].keys():  return 0.0
+  if piece not in board[color].keys():
+    return 0.0
 
   clone_board = copy.deepcopy(board)
   del clone_board[color][piece]
 
-  positions_with    = set([ (x['new_position'][0],x['new_position'][1]) for x in get_moves(board,opposite[color]      ) ])
-  positions_without = set([ (x['new_position'][0],x['new_position'][1]) for x in get_moves(clone_board,opposite[color]) ])
+  positions_with = set([(x['new_position'][0], x['new_position'][1]) for x in get_moves(board, OPPOSITE[color])])
+  positions_without = set([(x['new_position'][0], x['new_position'][1]) for x in get_moves(clone_board, OPPOSITE[color])])
 
   positions_diff = positions_without - positions_with
 
@@ -146,18 +170,11 @@ def shielding(board,color,piece):
 
   value  = 0.0
 
+  """now if I remove the piece and try to find out what moves are possible.... 
+  and then I put the piece back.. and find out what moves are possible... so the differece can show me 
+  what pieces are actually being protected from this piece.."""
   for x in positions_diff:
     if x in rboard[color].keys():
-      value= value + piece_value[rboard[color][x]]
-
+      value += PIECE_VALUE[rboard[color][x]]
 
   return value
-  
-
-  ## now if I remove the piece and try to find out what moves are possible.... 
-  ## and then I put the piece back.. and find out what moves are possible... so the differece can show me 
-  ## what pieces are actually being protected from this piece..
-  
-
-
-
