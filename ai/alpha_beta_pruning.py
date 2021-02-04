9d07d3b9c5db2b30a24e3c248f0d39244102589c
Sachin <skad5455@gmail.com>
2021-02-04 20:37:00 +0530

    Fixed algo

--- ai/alpha_beta_pruning.py
+++ ai/alpha_beta_pruning.py
@@ -28,8 +28,8 @@ def alpha_beta_pruning(board, color, depth):
 
   piece, x, y = out[0].decode('utf-8').split(" ")
   piece = mm[int(piece)]
-  x = int(x) +1
-  y = int(y) +1
+  x = int(x)
+  y = int(y)
 
   print(piece , x, y)
 
@@ -40,7 +40,7 @@ def alpha_beta_pruning(board, color, depth):
   }
 
 
-def alpha_beta_pruning_native(board,color,depth):
+def alpha_beta_pruning_native(board, color, depth):
   if depth == 0:
     return evaluate_board(board, color)
 
@@ -58,48 +58,36 @@ def alpha_beta_pruning_native(board,color,depth):
 
   for move in moves_list:
     clone_board = helper.generate_board(board, move)
-    score = alpha_beta_min(clone_board, OPPOSITE[color], alpha, beta, depth)
+    score = alpha_beta(clone_board, OPPOSITE[color], alpha, beta, depth - 1, True)
     if score > best_score:
       best_move = move
       best_score = score
 
   total = time.time() - st
   print(total)
-  print(best_move)
+  print(best_move, best_score)
 
   return best_move
 
 
-def alpha_beta_min(board, color, alpha, beta, depth):
-  if depth == 0 or helper.game_over(board, color) :
-      return evaluate_board(board, color)
+def alpha_beta(board, color, alpha, beta, depth, isMin = False):
+  if depth == 0 or helper.game_over(board, color):
+    return evaluate_board(board, color)
 
   moves_list = helper.get_moves(board, color)
 
+  score = float('inf') if isMin else float('-inf')
   for move in moves_list:
     clone_board = helper.generate_board(board, move)
+    if not isMin:
+      score = max(score, alpha_beta(clone_board, OPPOSITE[color], alpha, beta, depth - 1, True))
+      alpha = max(alpha, score)
+      if beta <= alpha:
+        return score
+    else:
+      score = min(score, alpha_beta(clone_board, OPPOSITE[color], alpha, beta, depth - 1, False))
+      beta = min(beta, score)
+      if beta <= alpha:
+        return score
 
-    score = alpha_beta_max(clone_board, OPPOSITE[color], alpha, beta, depth-1)
-    if score <= beta:
-      return alpha
-    if score < alpha:
-      beta = score
-
-  return beta
-
-def alpha_beta_max(board, color, alpha, beta, depth):
-  if depth == 0 or helper.game_over(board, color) :
-      return evaluate_board(board, color)
-
-  moves_list = helper.get_moves(board, color)
-
-  for move in moves_list:
-    clone_board = helper.generate_board(board, move)
-
-    score = alpha_beta_min(clone_board, OPPOSITE[color], alpha, beta, depth-1)
-    if score >= beta:
-      return beta
-    if score > alpha:
-      alpha = score
-
-  return alpha
+  return score
