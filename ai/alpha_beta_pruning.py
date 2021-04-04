bb16dd19aee9804b0a1b239b00ff99e285d7fd96
Sachin <skad5455@gmail.com>
2021-04-04 15:43:27 +0530

    Added cache

--- ai/alpha_beta_pruning.py
+++ ai/alpha_beta_pruning.py
@@ -1,11 +1,17 @@
 import subprocess
 import time
 
+from collections import defaultdict
+
 from ai.cpu import evaluate_board
 from common.constants import OPPOSITE, BLACK
 
 
-def alpha_beta_pruning(board, color, depth):
+def transform_board_to_string(board):
+  """
+  Transforms board piece positions into line-separated "<y> <x>" strings
+  """
+
   data = ""
   piece_order = ["k", "q", "b", "n", "r", "p"]
   piece_codes = ["k", "q", "b", "b", "n", "n", "r", "r", "p", "p", "p", "p", "p", "p", "p", "p"]
@@ -26,8 +32,8 @@ def alpha_beta_pruning(board, color, depth):
     "r": [],
     "p": []
   }
-  user_pieces = [_p for _p in board.pieces if _p.color != color]
-  cpu_pieces = [_p for _p in board.pieces if _p.color == color]
+  user_pieces = [_p for _p in board.pieces if _p.color == board.user_color]
+  cpu_pieces = [_p for _p in board.pieces if _p.color != board.user_color]
 
   for piece in user_pieces:
     user_piece_positions[piece.type].append(piece.position)
@@ -73,20 +79,26 @@ def alpha_beta_pruning(board, color, depth):
       for _ in range(num_killed):
         data += '-1 1\n'
 
+  return data
+
+
+def alpha_beta_pruning(board, color, depth):
+  """
+  Returns the best move calculated using the C++ compiled binary
+  """
+
+  st = time.time()
+
   player = 16
-  # print(data)
   proc = subprocess.Popen(
     ["ai/minimax", str(player), str(depth)],
     stdout=subprocess.PIPE,
     stdin=subprocess.PIPE
   )
-
-  st = time.time()
+  data = transform_board_to_string(board)
   out = proc.communicate(data.encode("utf-8"))
   total = time.time() - st
-  print(total)
 
-  print(out[0].decode('utf-8').split(" "))
   old_pos, piece, x, y, score = out[0].decode('utf-8').split(" ")
   piece = piece_codes[int(piece) - player]
   x, y, old_pos = int(x), int(y), int(old_pos)
@@ -97,14 +109,23 @@ def alpha_beta_pruning(board, color, depth):
     old_y, old_x = 7 - old_y, 7 - old_x
 
   move = {'color': color, 'piece': piece, 'new_position': [y, x], 'old_position': [old_y, old_x]}
+
+  print(total)
+  print(out[0].decode('utf-8').split(" "))
   print(move, score)
 
   return move
 
 
 def alpha_beta_pruning_native(board, color, depth):
+  """
+  Returns the best move using Pythonic alpha-beta optimized minimax implementation
+  """
+
   print("Calculating...")
   st = time.time()
+
+  score_cache = defaultdict()
   moves_list = board.get_moves(color)
 
   if len(moves_list) == 0 or depth == 0:
@@ -118,7 +139,7 @@ def alpha_beta_pruning_native(board, color, depth):
 
   for move in moves_list:
     clone_board = board.clone(move)
-    score = alpha_beta_min(clone_board, OPPOSITE[color], alpha, beta, depth - 1)
+    score = alpha_beta_min(clone_board, OPPOSITE[color], alpha, beta, depth - 1, score_cache)
     if score > best_score:
       best_move = move
       best_score = score
@@ -127,44 +148,63 @@ def alpha_beta_pruning_native(board, color, depth):
   print(total)
   print(best_move, best_score)
 
+  from pprint import pprint
+  pprint(score_cache)
+
   return best_move
 
 
-def alpha_beta_min(board, color, alpha, beta, depth):
+def alpha_beta_min(board, color, alpha, beta, depth, score_cache):
   if depth == 0:
-    return -1 * evaluate_board(board)
+    board_str = transform_board_to_string(board)
+    if score_cache.get(board_str) is None:
+      score_cache[board_str] = evaluate_board(board)
+
+    return score_cache[board_str]
 
   moves_list = board.get_moves(color)
 
-  score = float('inf')
   for move in moves_list:
     clone_board = board.clone(move)
 
-    score = min(
-      score,
-      alpha_beta_max(clone_board, OPPOSITE[color], alpha, beta, depth - 1)
-    )
-    beta = min(beta, score)
-    if beta <= alpha:
-      return score
-  return score
+    score = alpha_beta_max(clone_board, OPPOSITE[color], alpha, beta, depth - 1, score_cache)
+
+    if score <= alpha:
+      board_str = transform_board_to_string(clone_board)
+      score_cache[board_str] = alpha
+      return score_cache[board_str]
+
+    if score < beta:
+      beta = score
+
+  board_str = transform_board_to_string(board)
+  score_cache[board_str] = beta
+  return score_cache[board_str]
 
 
-def alpha_beta_max(board, color, alpha, beta, depth):
+def alpha_beta_max(board, color, alpha, beta, depth, score_cache):
   if depth == 0:
-    return evaluate_board(board)
+    board_str = transform_board_to_string(board)
+    if score_cache.get(board_str) is None:
+      score_cache[board_str] = -evaluate_board(board)
+
+    return score_cache[board_str]
 
   moves_list = board.get_moves(color)
 
-  score = float('-inf')
   for move in moves_list:
     clone_board = board.clone(move)
 
-    score = max(
-      score,
-      alpha_beta_min(clone_board, OPPOSITE[color], alpha, beta, depth - 1)
-    )
-    alpha = max(score, alpha)
-    if beta <= alpha:
-      return score
-  return score
+    score = alpha_beta_min(clone_board, OPPOSITE[color], alpha, beta, depth - 1, score_cache)
+
+    if score >= beta:
+      board_str = transform_board_to_string(clone_board)
+      score_cache[board_str] = beta
+      return score_cache[board_str]
+
+    if score > alpha:
+      alpha = score
+
+  board_str = transform_board_to_string(board)
+  score_cache[board_str] = alpha
+  return score_cache[board_str]
